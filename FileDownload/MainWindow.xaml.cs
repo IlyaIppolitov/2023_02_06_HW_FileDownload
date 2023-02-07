256834d45d61d9bdf4aefe9d532002d61a39acd8
FinT08 <IppolitovIS@gmail.com>
2023-02-07 22:24:35 +0300

    Добавьте файлы проекта.

---
+++ FileDownload/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LibFileDownload;
+
+namespace FileDownload
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public string downloadFileName = @"https://github.com/rodion-m/SystemProgrammingCourse2022/raw/master/files/payments_19mb.zip";
+        public string saveFileName = @"D:\FilesToRead\payments_19mb.zip";
+        CancellationTokenSource cts = new CancellationTokenSource();
+
+        private async void buttonDownload_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await DownloadToFile.DownloadFileToPathAsync(downloadFileName, saveFileName, cts);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonCancel_Click(object sender, RoutedEventArgs e)
+        {
+            cts.Cancel();
+        }
+    }
+}
---
+++ LibFileDownload/DownloadToFile.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace LibFileDownload
+{
+    public class DownloadToFile
+    {
+        public static async Task DownloadFileToPathAsync(string fileName, string outFullPath, CancellationTokenSource cts)
+        {
+            // Проверка не нулевых вхожных данных
+            if (string.IsNullOrEmpty(fileName)) throw new ArgumentNullException("fileName");
+            if (string.IsNullOrEmpty(outFullPath)) throw new ArgumentNullException("fullPathWhereToSave");
+
+            // Поток для сохранения файла
+            await using var fileStream = File.OpenWrite(outFullPath);
+            // Определение HttpClient
+            using var httpClient = new HttpClient();
+
+            try
+            {
+                // Получение ответа по факту прочтения заголовков (содержимое ещё не прочитано)
+                using var responseFile = await httpClient.GetAsync(
+                    fileName, HttpCompletionOption.ResponseHeadersRead, cts.Token).ConfigureAwait(false);
+
+                // Проверка кода ответа
+                responseFile.EnsureSuccessStatusCode();
+                if (responseFile.StatusCode != System.Net.HttpStatusCode.OK)
+                    throw new System.Net.WebException($"Код ответа не соответствует ожидаемому ОК, полученный код ответа: {responseFile.StatusCode}");
+
+
+                // ReadAsStreamAsync() -    Сериализует HTTP-содержимое и возвращает поток,
+                //                          представляющий содержимое в асинхронной операции.
+                await using var contentPart = await responseFile.Content.ReadAsStreamAsync(cts.Token).ConfigureAwait(false);
+
+                var fileLen = responseFile.Content.Headers.ContentLength;
+                if (fileLen == 0) throw new ArgumentException("File has no length!");
+
+
+                // Определяем буфер (не знаю почему такой размер =)
+                var buffer = new byte[1024 * 8 * 2];
+                // Условно бесконечный цикл с выходов в случае чтения 0 байт
+                while (true)
+                {
+                    int bytesRead = await contentPart.ReadAsync(buffer);
+                    if (bytesRead == 0)
+                    {
+                        break;
+                    }
+                    Console.WriteLine(bytesRead);
+
+                    if (bytesRead == buffer.Length)
+                    {
+                        await fileStream.WriteAsync(buffer);
+                    }
+                    else
+                    {
+                        await fileStream.WriteAsync(buffer.Take(bytesRead).ToArray());
+                    }
+                }
+
+            }
+            catch (TaskCanceledException) when (cts.IsCancellationRequested)
+            {
+                throw new System.Net.WebException($"Загрузка была прервана пользователем!");
+            }
+            catch (TaskCanceledException)
+            {
+                throw new System.Net.WebException($"Не знаю чего случилось, но загрузка прервана!");
+            }
+        }
+    }
+}
